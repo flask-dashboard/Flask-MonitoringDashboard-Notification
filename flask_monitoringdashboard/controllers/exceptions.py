98a48a1bd29cc52f65ad6fd158b2ef33099163f4
Carmen Alberte Nielsen <alcn@itu.dk>
2025-02-18 11:37:56 +0100

    Fix descriptions and names

--- flask_monitoringdashboard/controllers/exceptions.py
+++ flask_monitoringdashboard/controllers/exceptions.py
@@ -1,11 +1,20 @@
 from flask_monitoringdashboard.database.exception_info import get_exceptions_with_timestamps, get_exceptions_with_timestamps_and_stacktrace_id
-from flask_monitoringdashboard.database.full_stack_trace import get_code_from_stacktrace_id
+from flask_monitoringdashboard.database.full_stack_trace import get_stacklines_from_stacktrace_id
 
 def get_exceptions_with_timestamp(session, offset, per_page):
     """
+    Gets information about exceptions including timestamps of latest and first occurence. 
     :param session: session for the database
-    :param endpoints: a list of endpoints, encoded by their name
-    :return: for every endpoint in endpoints, a list with the performance
+    :param offset: number of items to skip
+    :param per_page: number of items to return
+    :return: A list of dicts. Each dict contains:
+             - exception_type (str)
+             - exception_msg (str)
+             - endpoint name (str)
+             - endpoint_id (int)
+             - latest_timestamp (datetime)
+             - first_timestamp (datetime)
+             - count (int) representing the number of occurrences.
     """
     
     return [
@@ -22,6 +31,24 @@ def get_exceptions_with_timestamp(session, offset, per_page):
     ]
 
 def get_detailed_exception_info(session, offset, per_page, endpoint_id):
+    """
+    Gets detailed information about exceptions on an endpoint (including stack trace).
+    :param session: session for the database
+    :param offset: number of items to skip
+    :param per_page: number of items to return
+    :param endpoint_id: the id of the endpoint
+    :return: A list of dicts. Each dict contains:
+             - type (str) of the exception
+             - message (str) of the exception
+             - full_stacktrace (lst of dicts) Each dict contains:
+                - code (str)
+                - filename (str)
+                - line_number (int)
+                - function_name (str)
+             - latest_timestamp (datetime)
+             - first_timestamp (datetime)
+             - count (int) representing the number of occurrences.
+    """
     return [
         {
             'type': exception.exception_type, 
@@ -33,7 +60,7 @@ def get_detailed_exception_info(session, offset, per_page, endpoint_id):
                     'line_number': exceptionStackLine.code.line_number,
                     'function_name': exceptionStackLine.code.function_name
                 }
-                for exceptionStackLine in get_code_from_stacktrace_id(session, exception.full_stack_trace_id)],
+                for exceptionStackLine in get_stacklines_from_stacktrace_id(session, exception.full_stack_trace_id)],
             'latest_timestamp': exception.latest_timestamp,
             'first_timestamp': exception.first_timestamp,
             'count': exception.count
--- flask_monitoringdashboard/frontend/js/controllers/exceptionInfo.js
+++ flask_monitoringdashboard/frontend/js/controllers/exceptionInfo.js
@@ -1,33 +1,17 @@
-// dette er frontend, husk "npm run dev" fra frontend/ for at kunne se dine ændringer
-// Opsætning første gang: npm install --save-dev webpack webpack-cli babel-loader @babel/core @babel/preset-env
-
-export function ExceptionController ($scope, $http, menuService, paginationService, endpointService) {
+export function ExceptionController($scope, $http, menuService, paginationService, endpointService) {
     endpointService.reset();
-    // Hvis værdien 'new_dashboard' nedenfor ændres skal den også ændres i menuService, men den bruges ingen andre steder
-    menuService.reset('new_dashboard'); // Fokus når man klikker på den i menuen (den bliver hvid og de andre bliver grå)
+    menuService.reset('exception_overview'); 
 
-    // Route http://127.0.0.1:4200/dashboard/new_dashboard sættes i frontend/app.js
+    $scope.table = [];
 
-    $scope.table = []; // scope bruges i static/pages/new_dashboard.html
-    // Hvis static/pages/new_dashboard.html filen skal renames, skal frontend/app.js lige opdateres
-
-    /* 
-        API og frontend virker til at køre på samme port så vidt jeg har forstået, og API routing specificeres i view layer
-
-        filer der muliggør API lige nu (fra bunden og op):
-            database/exception_info -> controllers/exceptions -> views/excpetion.py --api--> frontend/js/controllers/exceptionInfo.js
-       
-    */
     paginationService.init('exceptions');
     $http.get('api/num_exceptions').then(function (response) {
         paginationService.setTotal(response.data);
     });
 
     paginationService.onReload = function () {
-        // formService.isLoading = true; ?
         $http.get('api/exception_info/' + paginationService.getLeft() + '/' + paginationService.perPage).then(function (response) {
             $scope.table = response.data;
-            // formService.isLoading = false; ?
         });
     };
 };
