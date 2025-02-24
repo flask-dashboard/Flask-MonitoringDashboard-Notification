3fda0c1d0c0579dbf396fd14259284891ce47d6a
Natalie Clara Petersen <natp@itu.dk>
2025-02-24 17:01:10 +0100

    string key, og highlightElement()

--- flask_monitoringdashboard/frontend/js/controllers/endpointException.js
+++ flask_monitoringdashboard/frontend/js/controllers/endpointException.js
@@ -26,14 +26,22 @@ export function EndpointExceptionController ($scope, $http, menuService, paginat
         });
     };
 
-    $scope.getFunctionById = async function (function_id, stack_trace_id) {
-        let key = [function_id, stack_trace_id];
+    $scope.getUniqueKey = function (function_id, stack_trace_id) {
+        return `code_${function_id}_${stack_trace_id}`;
+    }
+
+    $scope.getFunctionById = function (function_id, stack_trace_id) {
+        let key = $scope.getUniqueKey(function_id, stack_trace_id);
+        
         if ($scope.id2Function[key] === undefined){
-            await $http.get(`api/function_definition/${function_id}/${stack_trace_id}`)
+            $http.get(`api/function_definition/${function_id}/${stack_trace_id}`)
                 .then((response) => {
                     $scope.id2Function[key] = response.data;
-                })
+                    $scope.$applyAsync(() => {
+                        let element = document.getElementById(key);
+                        Prism.highlightElement(element);
+                    });
+                });
         }
-        Prism.highlightAll();
     }
 };
