flask-dashboard/Flask-MonitoringDashboard

Add unit tests for exceptions in the database. exception_info still missing.

tests/unit/database/test_exception_info.py
@@ -1,0 +1,17 @@
+
+"""
+This file contains all unit tests of exception info in the database.
+(Corresponding to the file: 'flask_monitoringdashboard/database/exception_info.py')
+"""
+from flask_monitoringdashboard.database import ExceptionInfo
+from flask_monitoringdashboard.database.exception_info import get_exception_info, add_exception_info, count_grouped_exceptions, count_endpoint_grouped_exceptions, get_exceptions_with_timestamps, delete_exception, get_exceptions_with_timestamps_and_stacktrace_id
+
+""" #The below code is failing because stacktrace is not relational to exception info and therefore the model cannot be created with a full stack trace object as a parameter
+def test_get_exception_info(session, exception_info):
+    e_info = get_exception_info(session, exception_info.request_id)
+    assert e_info.request_id == exception_info.request_id
+    assert e_info.exception_type_id == exception_info.exception_type_id
+    assert e_info.exception_msg_id == exception_info.exception_msg_id
+    assert e_info.full_stack_trace_id == exception_info.full_stack_trace_id
+
+"""

tests/unit/database/test_exception_stack_line.py
@@ -1,0 +1,13 @@
+
+"""
+This file contains all unit tests of exception stack line in the database.
+(Corresponding to the file: 'flask_monitoringdashboard/database/exception_stack_line.py')
+"""
+from flask_monitoringdashboard.database import ExceptionStackLine
+from flask_monitoringdashboard.database.exception_stack_line import add_exception_stack_line
+
+def test_add_exception_stack_line(session, full_stack_trace, function_definition, code_line):
+    assert session.query(ExceptionStackLine).filter(ExceptionStackLine.full_stack_trace_id == full_stack_trace.id).one_or_none() is None
+    add_exception_stack_line(session, full_stack_trace_id=full_stack_trace.id, position=0, code_line=code_line, function_defintion_id=function_definition.id, relative_lineno=1)
+    session.commit()
+    assert session.query(ExceptionStackLine).filter(ExceptionStackLine.full_stack_trace_id == full_stack_trace.id).one()

flask-dashboard/Flask-MonitoringDashboard-Notification

tests/unit/database/test_exception_info.py
@@ -1,0 +1,14 @@
+
+"""
+This file contains all unit tests of exception info in the database.
+(Corresponding to the file: 'flask_monitoringdashboard/database/exception_info.py')
+"""
+from flask_monitoringdashboard.database import ExceptionInfo
+from flask_monitoringdashboard.database.exception_info import get_exception_info, add_exception_info, count_grouped_exceptions, count_endpoint_grouped_exceptions, get_exceptions_with_timestamps, delete_exception, get_exceptions_with_timestamps_and_stack_trace_id
+
+def test_get_exception_info(session, exception_info):
+    e_info = get_exception_info(session, exception_info.request_id)
+    assert e_info.request_id == exception_info.request_id
+    assert e_info.exception_type_id == exception_info.exception_type_id
+    assert e_info.exception_msg_id == exception_info.exception_msg_id
+    assert e_info.stack_trace_snapshot_id == exception_info.stack_trace_snapshot_id

tests/unit/database/test_exception_stack_line.py
@@ -1,0 +1,13 @@
+
+"""
+This file contains all unit tests of exception stack line in the database.
+(Corresponding to the file: 'flask_monitoringdashboard/database/exception_stack_line.py')
+"""
+from flask_monitoringdashboard.database import ExceptionStackLine
+from flask_monitoringdashboard.database.exception_stack_line import add_exception_stack_line
+
+def test_add_exception_stack_line(session, stack_trace_snapshot, function_definition, code_line):
+    assert session.query(ExceptionStackLine).filter(ExceptionStackLine.stack_trace_snapshot_id == stack_trace_snapshot.id).one_or_none() is None
+    add_exception_stack_line(session, stack_trace_snapshot_id=stack_trace_snapshot.id, position=0, code_line=code_line, function_defintion_id=function_definition.id, relative_lineno=1)
+    session.commit()
+    assert session.query(ExceptionStackLine).filter(ExceptionStackLine.stack_trace_snapshot_id == stack_trace_snapshot.id).one()