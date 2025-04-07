65b620b0e16fd3bf0e67b6477b8bb23310b637aa
Natalie Clara Petersen <natp@itu.dk>
2025-04-07 18:28:11 +0200

    removed unused function:  create_codeline_from_frame

--- flask_monitoringdashboard/core/exceptions/stack_frame_parsing.py
+++ flask_monitoringdashboard/core/exceptions/stack_frame_parsing.py
@@ -2,7 +2,7 @@ import inspect
 from types import FrameType
 
 from flask_monitoringdashboard.core.exceptions.text_hash import text_hash
-from flask_monitoringdashboard.database import FunctionDefinition, CodeLine
+from flask_monitoringdashboard.database import FunctionDefinition
 
 
 def get_function_definition_from_frame(frame: FrameType) -> FunctionDefinition:
@@ -12,14 +12,3 @@ def get_function_definition_from_frame(frame: FrameType) -> FunctionDefinition:
     f_def.code_hash = text_hash(f_def.code)
     f_def.name = frame.f_code.co_name[:256]
     return f_def
-
-
-def create_codeline_from_frame(frame: FrameType, lineno):
-    c_line = CodeLine()
-    c_line.filename = frame.f_code.co_filename
-    c_line.line_number = lineno
-    c_line.function_name = frame.f_code.co_name
-    code_context = inspect.getframeinfo(frame).code_context
-    if code_context is not None and len(code_context) > 0:
-        c_line.code = code_context[0]
-    return c_line
