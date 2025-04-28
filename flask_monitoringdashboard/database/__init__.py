d032a205e19c8631a107278963a5f83576914006
Carmen Nielsen <carmennielsen@Carmens-MacBook-Air.local>
2025-04-28 10:06:37 +0200

    use back_populates instead of backref to have both attributes defined explicitly

--- flask_monitoringdashboard/database/__init__.py
+++ flask_monitoringdashboard/database/__init__.py
@@ -249,7 +249,7 @@ class StackTraceSnapshot(Base):
     id = Column(Integer, primary_key=True)
     hash = Column(String(64), nullable=False, unique=True)
 
-    stack_lines = relationship("ExceptionStackLine", backref="ExceptionOccurrence")
+    exception_stack_lines = relationship("ExceptionStackLine", back_populates="stack_trace_snapshot")
 
 
 class ExceptionType(Base):
@@ -362,7 +362,7 @@ class ExceptionStackLine(Base):
     stack_trace_snapshot_id = Column(
         Integer, ForeignKey(StackTraceSnapshot.id), primary_key=True
     )
-    stack_trace_snapshot = relationship(StackTraceSnapshot)
+    stack_trace_snapshot = relationship(StackTraceSnapshot, back_populates="exception_stack_lines")
     """Stack trace that belongs to this exc_stack_line."""
 
     exception_frame_id = Column(
